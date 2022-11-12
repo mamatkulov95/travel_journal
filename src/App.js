7ebd44bfc162f46519c15fc3b8b8a15578de8a42
Ma'rufjon <m.mamatkulov95@gmail.com>
2022-11-12 21:41:20 +0500

    [Card-section] -changed file structure and used object destructuring

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import Card from "./components/Card/Card";
 import "./App.css";
-import Card from "./Card/Card";
-import data from "../src/Card/CardData";
-function App() {
-  const travelCountry = data.map((item) => {
-    return <Card key={item.id} {...item} />;
-  });
 
-  return <div className="App">{travelCountry}</div>;
+function App() {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
 }
 
 export default App;
